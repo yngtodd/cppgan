6f1fbe7fa696ef87a516e7c923310aa960b85a91
yngtodd <young.todd.mk@gmail.com>
2019-03-01 00:21:17 -0500

    Maybe this will have to be on linux

--- build/lib/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/yngtodd/src/develop/cppgan/lib/gtest
-# Build directory: /Users/yngtodd/src/develop/cppgan/build/lib/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
